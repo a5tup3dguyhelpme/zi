99051c6b20643a37c2e95404dfd29a98367d45b9
a5tup3d_guy <juanpablohdz2202@gmail.com>
2026-09-04 21:18:20 -0600

    codigo en JS y texto ajustado

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+function finnis(){
+    document.getElementByClass("b").innerHTML = "hola, jotos ya llego elviolatoros por quien lloraban jsjskskskkakakak voy a hacjear un inche otso mue jejeje";
+   document.getElementByClass("c").innerHTML = "esta vez yo escribo el texto mamon jja nmms xd we AAA YA MERO REGRESO A LA SKUL W NO Q MIEDO AL CHILE NO JODAS @_@";
+}
