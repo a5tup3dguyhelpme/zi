b3321ebe2caba7ad37963214f5857c663cc696eb
a5tup3d_guy <juanpablohdz2202@gmail.com>
2026-09-04 21:31:02 -0600

    g

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function finnis(){
-    document.getElementByClass("b").innerHTML = "hola, jotos ya llego elviolatoros por quien lloraban jsjskskskkakakak voy a hacjear un inche otso mue jejeje";
+    document.getElementById("pepita").innerHTML = "hola, jotos ya llego elviolatoros por quien lloraban jsjskskskkakakak voy a hacjear un inche otso mue jejeje";
 }
