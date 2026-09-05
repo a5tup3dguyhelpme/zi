395092c4608d7b600e5e33494fbf217cfc9d2d4b
a5tup3d_guy <juanpablohdz2202@gmail.com>
2026-09-04 21:26:40 -0600

    asdksljñaL

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
 function finnis(){
     document.getElementByClass("b").innerHTML = "hola, jotos ya llego elviolatoros por quien lloraban jsjskskskkakakak voy a hacjear un inche otso mue jejeje";
 }
-function finnis(){
-     document.getElementByClass("c").innerHTML = "esta vez yo escribo el texto mamon jja nmms xd we AAA YA MERO REGRESO A LA SKUL W NO Q MIEDO AL CHILE NO JODAS @_@";
-}
