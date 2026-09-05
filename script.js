e4b8b00c5942d7cf560310ae0f1959b6a6a7402b
a5tup3d_guy <juanpablohdz2202@gmail.com>
2026-09-04 21:57:49 -0600

    se agregó input

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function finnis(){
     document.getElementById("pepita").innerHTML = "hola, jotos ya llego elviolatoros por quien lloraban jsjskskskkakakak voy a hacjear un inche otso mue jejeje";
 }
+async function pizza(){
+    const textoCorrectoW = document.getElementById("textoAca").value;
+    const textoAIngresar = "miranda01";
+    if (textoCorrectoW === textoAIngresar){
+        document.getElementById("nose").innerHTML = "pinche damian haz tus pinches clases pa q hagas cosas asi bien chingonsotas w"
+    }
+}
